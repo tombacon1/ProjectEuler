09a6e232416f4f23150e7c1f8da40a34932d5e66
Tom Bacon <tom.bacon1@gmail.com>
2023-12-11 20:31:49 +0100

    Updated challenge 19 with a simpler and clearer solution (Attempt2).

--- ProjectEuler - 19/Program.cs
+++ ProjectEuler - 19/Program.cs
@@ -18,14 +18,62 @@ internal class EulerProject19
     {
         Console.WriteLine(question);
         Console.WriteLine(separator);
+        int result;
+        
         Stopwatch sw = Stopwatch.StartNew();
 
+        result = Attempt1();
+
+        sw.Stop();
+        Console.WriteLine("Elapsed: " + sw.ElapsedMilliseconds + "ms");
+        Console.WriteLine("Result 1: " + result);
+
+        sw.Restart();
+
+        result = Attempt2();
+
+        sw.Stop();
+        Console.WriteLine("Elapsed: " + sw.ElapsedMilliseconds + "ms");
+        Console.WriteLine("Result 2: " + result);
+        Console.ReadLine();
+    }
+
+    private static int Attempt2()
+    {
+        int leapDay;
+        int curDay = 6;
+        int monthDays = 31;
+        int count = 0;
+
+        for (int year = 1901; year <= 2000; year++)
+        {
+            leapDay = IsLeapYear(year) ? 1 : 0;
+            for (int i = 0; i < daysInMonth.Length; i++)
+            {
+                monthDays = daysInMonth[i];
+                if (i == 1)
+                    monthDays += leapDay;
+
+                while (curDay < monthDays)
+                    curDay += DAYS_PER_WEEK;
+
+                curDay -= monthDays;
+                if (curDay == 1)
+                    count++;
+            }
+        }
+
+        return count;
+    }
+
+    private static int Attempt1()
+    {
         int lastSundayOfLastMonth = 30; // DEC 30 1900
         int daysLastMonth = 31;         // DEC 1900
         int leapDay = 0;
         int count = 0;
         int firstSunday = 0;
-        int daysThisMonth = 0;             
+        int daysThisMonth = 0;
 
         for (int year = 1901; year <= 2000; year++)
         {
@@ -33,8 +81,8 @@ internal class EulerProject19
             for (int i = 0; i < daysInMonth.Length; i++)
             {
                 daysThisMonth = daysInMonth[i];
-                if(i == 1) daysThisMonth += leapDay;
-                
+                if (i == 1) daysThisMonth += leapDay;
+
                 firstSunday = 7 - (daysLastMonth - lastSundayOfLastMonth);
                 if (firstSunday == 1) count++;
                 lastSundayOfLastMonth = ((daysThisMonth - firstSunday) / DAYS_PER_WEEK) * DAYS_PER_WEEK + firstSunday;
@@ -42,10 +90,7 @@ internal class EulerProject19
             }
         }
 
-        sw.Stop();
-        Console.WriteLine("Elapsed: " + sw.ElapsedMilliseconds + "ms");
-        Console.WriteLine("Result: " + count);
-        Console.ReadLine();
+        return count;
     }
 
     private static bool IsLeapYear(int year)
---
+++ ProjectEuler - 21/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+internal class EulerProject21
+{
+    static readonly string question = "Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).\r\n\r\n" + 
+        "If d(a) = b and d(b) = a, where a != b, then a and b are an amicable pair\r\n"  + 
+        "and each of a and b are called amicable numbers.\r\n\r\n" + 
+        "For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110;\r\n" + 
+        "therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.\r\n\r\n" + 
+        "Evaluate the sum of all the amicable numbers under 10000.";
+    static readonly string separator = new string('-', 50) + "\r\n";
+
+
+    static void Main()
+    {
+        Console.WriteLine(question);
+        Console.WriteLine(separator);
+        Stopwatch sw = Stopwatch.StartNew();
+
+
+
+
+        sw.Stop();
+        Console.WriteLine("Elapsed: " + sw.ElapsedMilliseconds + "ms");
+        Console.WriteLine("Result: ");
+        Console.ReadLine();
+    }
+}
