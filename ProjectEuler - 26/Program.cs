c22d7591171dfaca7b92a5fcd5a22873ca7c80ff
Tom Bacon <tom.bacon1@gmail.com>
2024-05-04 16:58:32 +0200

    Challenge 27 completed.

--- ProjectEuler - 26/Program.cs
+++ ProjectEuler - 26/Program.cs
@@ -2,10 +2,10 @@
 
 internal class EulerProject26
 {
-    static readonly string question = "A unit fraction contains 1 in the numerator.\r\n" + 
+    static readonly string question = "A unit fraction contains 1 in the numerator.\r\n" +
         "The decimal representation of the unit fractions with denominators 2 to 10 are given:\r\n\r\n" +
         "1/2 = 0.5\r\n1/3 =0.(3)\r\n1/4 =0.25\r\n1/5 = 0.2\r\n1/6 = 0.1(6)\r\n1/7 = 0.(142857)\r\n1/8 = 0.125\r\n1/9 = 0.(1)\r\n1/10 = 0.1\r\n\r\n" +
-        "Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be seen that 1/7 has a 6-digit recurring cycle.\r\n\r\n" + 
+        "Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be seen that 1/7 has a 6-digit recurring cycle.\r\n\r\n" +
         "Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.";
     static readonly string separator = new string('-', 50) + "\r\n";
 
@@ -21,10 +21,7 @@ internal class EulerProject26
         Dictionary<int, int> results = new Dictionary<int, int>();
 
         for (int i = 3; i < LIMIT; i++)
-        {
-            int len = FindRecurringCycle(i);
-            results.Add(i, len);
-        }
+            results.Add(i, GetRecurringCycleLength(i));
 
         KeyValuePair<int, int> maxCycle = results.MaxBy(r => r.Value);
 
@@ -34,10 +31,11 @@ internal class EulerProject26
         Console.ReadLine();
     }
 
-    private static int FindRecurringCycle(int d)
+
+    private static int GetRecurringCycleLength(int d)
     {
-        int[] seen = new int[d];
-        seen[1] = 1; 
+        int[] list = new int[d];
+        list[1] = 1;
         int i = 2;
         int n = 1;
         int rem = 0;
@@ -48,11 +46,13 @@ internal class EulerProject26
             rem = n % d;
             if (rem == 0)
                 return 0;
-            if (seen[rem] != 0)
-                return i - seen[rem];
-            seen[rem] = i;
+            if (list[rem] != 0)
+                return i - list[rem];
+            list[rem] = i;
             i++;
             n = rem;
         }
     }
 }
+
+
---
+++ ProjectEuler - 27/Program.cs
@@ -0,0 +1,97 @@
+﻿using System.Data;
+using System.Diagnostics;
+
+internal class EulerProject27
+{
+    static readonly string question = "   Euler discovered the remarkable quadratic formula:\r\n\r\n" +
+        "                                  n² + n + 41\r\n\r\n" +
+        "    It turns out that the formula will produce 40 primes for the consecutive\r\n" +
+        "   values n = 0 to 39. However, when n = 40, 40^2 + 40 + 41 = 40(40 + 1) + 41\r\n" +
+        "   is divisible by 41, and certainly when n = 41, 41² + 41 + 41 is clearly\r\n" +
+        "   divisible by 41.\r\n\r\n" +
+        "   The incredible formula  n² − 79n + 1601 was discovered, which produces 80\r\n" +
+        "   primes for the consecutive values n = 0 to 79. The product of the\r\n" +
+        "   coefficients, −79 and 1601, is −126479.\r\n\r\n" +
+        "   Considering quadratics of the form:\r\n\r\n" +
+        "     n² + an + b, where |a| < 1000 and |b| <= 1000\r\n\r\n" +
+        "     where |n| is the modulus/absolute value of n\r\n" +
+        "     e.g. |11| = 11 and |−4| = 4\r\n\r\n" +
+        "   Find the product of the coefficients, a and b, for the quadratic\r\n" +
+        "   expression that produces the maximum number of primes for consecutive\r\n" +
+        "   values of n, starting with n = 0.";
+    static readonly string separator = new string('-', 50) + "\r\n";
+    static readonly string tab = new string(' ', 4);
+
+
+    static void Main()
+    {
+        Console.WriteLine(question);
+        Console.WriteLine(separator);
+        Stopwatch sw = Stopwatch.StartNew();
+
+        List<Results> results = new List<Results>();
+        List<int> primesToOneThousand = new List<int>();
+
+        for (int i = 0; i <= 1000; i++)
+        {
+            if (IsPrime(i))
+                primesToOneThousand.Add(i);
+        }
+
+        for (int a = -999; a < 1000; a++)
+            foreach (int b in primesToOneThousand)
+            {
+                results.Add(new Results { a = a, b = b, count = CountQuadraticPrimes(a, b) });
+            }
+
+        Results result = results.MaxBy(r => r.count);
+
+        int product = result.a * result.b;
+
+        sw.Stop();
+        Console.WriteLine("Elapsed: " + sw.ElapsedMilliseconds + "ms");
+        Console.WriteLine("Result: \r\n" + tab + "a: " + result.a + "\r\n" + tab + "b: " + result.b + "\r\n" +  tab + "count: " + result.count + "\r\n" + tab + "product: " + product);
+        Console.ReadLine();
+    }
+
+    static long QuadraticEquation(int x, int a, int b)
+    {
+        return x * x + x * a + b;
+    }
+
+    static int CountQuadraticPrimes(int a, int b)
+    {
+        int n = 0;
+        while (true)
+        {
+            long res = QuadraticEquation(n, a, b);
+            if (!IsPrime(res))
+                return n;
+            n++;
+        }
+    }
+
+    static bool IsPrime(long n)
+    {
+        if (n == 2 || n == 3)
+            return true;
+
+        if (n <= 1 || n % 2 == 0 || n % 3 == 0)
+            return false;
+
+        for (int i = 5; i * i <= n; i += 6)
+        {
+            if (n % i == 0 || n % (i + 2) == 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    public struct Results
+    {
+        public int a { get; set; }
+        public int b { get; set; }
+        public int count { get; set; }
+    }
+}
