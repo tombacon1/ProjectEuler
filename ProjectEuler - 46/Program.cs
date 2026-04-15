4910f3efefc9d92780729655aabcb02ad3eb565e
Tom Bacon <tom.bacon1@gmail.com>
2026-04-15 21:03:14 +0200

    Completed solution for problem 46.

--- ProjectEuler - 46/Program.cs
+++ ProjectEuler - 46/Program.cs
@@ -1,10 +1,24 @@
-﻿using System.Diagnostics;
+﻿using System.Buffers.Binary;
+using System.Diagnostics;
 
 internal class EulerProject46
 {
-    static readonly string question = "";
+    static readonly string question = "It was proposed by Christian Goldbach that every odd composite number \r\n" + 
+                                      "can be written as the sum of a prime and twice a square.\r\n\r\n" +
+                                      "It turns out that the conjecture was false.\r\n\r\n" +
+                                      " 9 = 7 + 2 x 1^2" + "\r\n" +
+                                      "15 = 7 + 2 x 2^2" + "\r\n" +
+                                      "21 = 3 + 2 x 3^2" + "\r\n" +
+                                      "25 = 7 + 2 x 3^2" + "\r\n" +
+                                      "27 = 19 + 2 x 2^2" + "\r\n" +
+                                      "33 = 31 + 2 x 1^2" + "\r\n\r\n" +
+                                      "What is the smallest odd composite that cannot be written as the sum \r\n" +
+                                      "of a prime and twice a square?";
     static readonly string separator = new string('-', 50) + "\r\n";
 
+    static int _n = 1;
+    static int _p = 2;
+    static List<int> Primes = new List<int>();
 
     static void Main()
     {
@@ -24,8 +38,70 @@ internal class EulerProject46
     {
         internal static int Solve()
         {
-            throw new NotImplementedException();
+            while(true)
+            {
+                
+                _n = GetNextOddComposite();
+                while(_p < _n)
+                {
+                    Primes.Add(_p);
+                    _p = GetNextPrime();
+                }
+
+                bool match = true;
+
+                foreach(int prime in Primes){
+                    int candidate = prime + 2;
+                    for (int i = 2; candidate <= _n; i++)
+                    {
+                        if (candidate == _n)
+                        {
+                            match = false;
+                            break;
+                        }
+
+                        candidate = prime + 2 * i * i;
+                    }
+                }
+
+                if (match)
+                    return _n;
+            }
+        }
+
+        internal static int GetNextOddComposite()
+        {
+            int next = _n + 2;
+            while (IsPrime(next) || next % 2 == 0)
+                next += 2;
+            return next;
+        }
+
+        internal static int GetNextPrime()
+        {
+            if (_p < 2) return 2;
+            if (_p == 2) return 3;
+
+            int next = _p + 1;
+            while (!IsPrime(next))
+                next++;
+
+            return next;
+        }
+
+        internal static bool IsPrime(int n)
+        {
+            if (n < 2) return false;
+            if (n == 2) return true;
+            if (n % 2 == 0) return false;
+
+            for (int i = 3; i <= Math.Sqrt(n); i += 2)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+
+            return true;
         }
     }
 }
-
